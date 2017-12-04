1681949c54da31a8fe3ae00f2dd52c5ad71c56c7
Thomas Fauskanger <thomasfauskanger@gmail.com>
2017-12-04 02:41:15 +0100

    fix exceptions.py multiple inheritence

--- mypolr/exceptions.py
+++ mypolr/exceptions.py
@@ -22,13 +22,13 @@ class MypolrError(Exception):
     """
 
 
-class DebugTempWarning(Warning, MypolrError):
+class DebugTempWarning(MypolrError):
     """Temporary Warning that should be removed"""
     def __init__(self):
         super(DebugTempWarning, self).__init__('This should not happen')
 
 
-class CustomEndingUnavailable(ValueError, MypolrError):
+class CustomEndingUnavailable(MypolrError):
     """Raised when a custom ending is in use and therefore cannot be created.
 
     :param str custom_ending: the custom ending that was unavailable
@@ -38,21 +38,21 @@ class CustomEndingUnavailable(ValueError, MypolrError):
         super(CustomEndingUnavailable, self).__init__(msg)
 
 
-class BadApiRequest(requests.HTTPError, MypolrError):
+class BadApiRequest(MypolrError):
     """Raised when a request is malformed or otherwise is not understandable by server."""
     def __init__(self):
         msg = 'HTTP 400 Bad Request: Request malformed, or arguments do not fit the required data type.'
         super(BadApiRequest, self).__init__(msg)
 
 
-class BadApiResponse(ValueError, MypolrError):
+class BadApiResponse(MypolrError):
     """Raised when a response is malformed and cannot be interpreted as valid JSON."""
     def __init__(self):
         msg = 'Cannot interpret API response: invalid JSON.'
         super(BadApiResponse, self).__init__(msg)
 
 
-class UnauthorizedKeyError(requests.HTTPError, MypolrError):
+class UnauthorizedKeyError(MypolrError):
     """Raised when an invalid key has been used in a request.
 
     This refers either to:
@@ -66,14 +66,14 @@ class UnauthorizedKeyError(requests.HTTPError, MypolrError):
         super(UnauthorizedKeyError, self).__init__('HTTP 401 Unauthorized: {}'.format(msg))
 
 
-class QuotaExceededError(requests.HTTPError, MypolrError):
+class QuotaExceededError(MypolrError):
     """Admins may assign quotas to users, and this is raised when it's exceeded and service stopped."""
     def __init__(self):
         msg = 'HTTP 403 Forbidden: quota is exceeded.'
         super(QuotaExceededError, self).__init__(msg)
 
 
-class ServerOrConnectionError(requests.ConnectionError, MypolrError):
+class ServerOrConnectionError(MypolrError):
     """Raised when there is a timeout, internal server error, or any other connection error."""
     def __init__(self):
         msg = 'API cannot be reached. Check connection or server status.'
