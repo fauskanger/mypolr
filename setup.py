e7cdc1e334af55e78dfd453eb92ecd559f3e34f5
Thomas Fauskanger <thomasfauskanger@gmail.com>
2017-12-12 01:22:47 +0100

    fix setup.py description for pypi

--- setup.py
+++ setup.py
@@ -6,9 +6,7 @@ from setuptools import setup
 # from codecs import open
 # from os import path
 
-short_description = "Package to easily create and manage short links using the " \
-                    "`Polr Project <https://polrproject.org>`_'s REST " \
-                    "`API <https://docs.polrproject.org/en/latest/developer-guide/api/>`_ that also has CLI support."
+short_description = "Package to use with installations of the Polr Project 's REST API. Can also be used from CLI."
 
 # here = path.abspath(path.dirname(__file__))
 # with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
